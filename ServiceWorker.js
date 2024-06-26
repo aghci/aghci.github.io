4d9e6a689d13d86a5768e228436d0c80a9cc6220
aghci <173868098@qq.com>
2024-06-26 16:47:34 +0800

    添加 交易 接口

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/f5bd86fd9d3218f3276dc9f403e26b58.framework.js",
-    "Build/8aaf3bc4b31a2d54586218d570fe4f5f.data",
-    "Build/9d0f0aac2b5b059978e218a5690ddeeb.wasm",
+    "Build/0c0875415baf50d483849ae0422fdeba.framework.js",
+    "Build/f4920bceac9f529b32551903db3b3cd1.data",
+    "Build/1302e41c9eb6dffa586ca9dad1461712.wasm",
     "TemplateData/style.css"
 
 ];
