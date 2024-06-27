03050ed02a6db19d19e04d713f2c36ec0fd9279a
aghci <173868098@qq.com>
2024-06-27 18:19:24 +0800

    添加 复制

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/2e7e74c65feddec6be8d529e49e4417e.framework.js",
-    "Build/e7da38f6e1ea8b5be0ae9fcacd1e0a21.data",
-    "Build/0393a44578b198c139a64dfd48030170.wasm",
+    "Build/deb27193209444adcf177afe053c6636.framework.js",
+    "Build/8790abab428156822fa857be7e55e805.data",
+    "Build/e17ad1682a6a796f52ea6bddfad7e3a0.wasm",
     "TemplateData/style.css"
 
 ];
