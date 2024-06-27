f9345e6fb003cb21380400cfa904cbbe3994b5aa
aghci <173868098@qq.com>
2024-06-27 17:58:58 +0800

    是

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/b94cebbc08663beda03599b0b822d202.framework.js",
-    "Build/fc8c32dfaa0ca0522e9e2f640adf33ed.data",
-    "Build/ab62508daf925f79c03736fb514c0c65.wasm",
+    "Build/2e7e74c65feddec6be8d529e49e4417e.framework.js",
+    "Build/e7da38f6e1ea8b5be0ae9fcacd1e0a21.data",
+    "Build/0393a44578b198c139a64dfd48030170.wasm",
     "TemplateData/style.css"
 
 ];
