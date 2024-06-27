3d86c879a7e5cd72532b1bbc3563f3d52c8ed453
aghci <173868098@qq.com>
2024-06-27 17:24:29 +0800

    修复 错误

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/425c5b53491ee068cf55d93f528e6c86.framework.js",
-    "Build/7315ed6a3426223f9c40b177e2a53255.data",
-    "Build/3cb98f3a3343587d3c7350e4896910f9.wasm",
+    "Build/f35723f939dd8477cd3bf39b7b37ab8d.framework.js",
+    "Build/382355bf4133fd97d2f885c79f00f9b9.data",
+    "Build/e5f9fde635be3aec8377ec4471eb5d06.wasm",
     "TemplateData/style.css"
 
 ];
