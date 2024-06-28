c5170936afdc806331af1c1f3fa1e0d8ef2dac39
aghci <173868098@qq.com>
2024-06-28 16:53:40 +0800

    分享 测试 代码

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/33cdfb3ad86ab8308ed5c403f3fec807.framework.js",
-    "Build/02f7a1e13314f2a9eb3d979fb2cd6530.data",
-    "Build/e97cbf343a11f57e1211b503c31981db.wasm",
+    "Build/c3ee4de9fdec2b5717eedbe2538ce046.framework.js",
+    "Build/a4ffa2db4379452fb48da6cfec4eda95.data",
+    "Build/fe6a437ffd620886ed7a24c04b913c5d.wasm",
     "TemplateData/style.css"
 
 ];
