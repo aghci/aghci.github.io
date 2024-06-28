7cae2c3c0e9a25ca0b44916af580913a06605ef4
aghci <173868098@qq.com>
2024-06-28 14:39:30 +0800

    调整输出

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/43538fe1c42fce663a4dadde06a65f26.framework.js",
-    "Build/ba53c5480811b517aabdeef59ad4bcbc.data",
-    "Build/ed751f845fd81a489d06178ffd9b9298.wasm",
+    "Build/c993e6cc2e49c8d96f63bef7265ba479.framework.js",
+    "Build/54e0bc7256657ebe70aee09220f8799e.data",
+    "Build/7bfb6b9805ccc4d93a2e1bb10b713f2a.wasm",
     "TemplateData/style.css"
 
 ];
