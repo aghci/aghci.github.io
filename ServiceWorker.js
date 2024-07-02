a6043185eae0f11d03bec37dd6d7b33859a593fa
aghci <173868098@qq.com>
2024-07-02 12:28:41 +0800

    修复 按钮 大小

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/c1663b7e43b0bf12c84f333c7d31c88c.framework.js",
-    "Build/0993a4033b53c646235028ffd46b2713.data",
+    "Build/dec3a51b32d3a795a5ad56497889c5bd.data",
     "Build/36594c6bc3029f266b716d9ea2c04501.wasm",
     "TemplateData/style.css"
 
