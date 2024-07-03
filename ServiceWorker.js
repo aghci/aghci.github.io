db22d4ecc4537ef2bc15bebcb4e3f7fa92a50c80
aghci <173868098@qq.com>
2024-07-03 15:52:39 +0800

    打包-Debug

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Ton-1.0";
+const contentToCache = [
+    "Build/0145c752a950e797e3f5c26444736e72.loader.js",
+    "Build/2ed5662014a1f1dabe9c44214fbd7851.framework.js",
+    "Build/ed476f332242cb8be362578b0f1cb91f.data",
+    "Build/c49973a4b00d43e3fe7472f215a03fc1.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
