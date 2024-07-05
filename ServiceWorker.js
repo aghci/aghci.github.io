8b2073b22be7bc2eefd85cbdbd662d31b0f001d5
aghci <173868098@qq.com>
2024-07-05 20:03:16 +0800

    打包-Debug

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Ton-1.0";
 const contentToCache = [
     "Build/0145c752a950e797e3f5c26444736e72.loader.js",
-    "Build/ead8c265226901f07fd3048ab984b0c8.framework.js",
-    "Build/47b28d86e8e0183ce0019c81895d9b29.data",
-    "Build/44b641d6a67da7f79b5f5b4d6b2b9daf.wasm",
+    "Build/8cab898fa8ad91f47322e36ffa402e73.framework.js",
+    "Build/42fd197c0cf818ae7e8fc57253f30d15.data",
+    "Build/85489683e0b1ad7258748e17ba5d7440.wasm",
     "TemplateData/style.css"
 
 ];
